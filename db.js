bb230f5c6f07a157628d5499d1cbd321bd4715c2
Nicholas L <NLoo1@outlook.com>
2024-04-11 12:30:10 -0400

    Add save() to reservation, fullName to customer

--- db.js
+++ db.js
@@ -1,9 +1,26 @@
 /** Database for lunchly */
 
-const pg = require("pg");
+const { Client } = require('pg');
 
-const db = new pg.Client("postgresql:///lunchly");
+let DB_URI;
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "lunchly_test"
+} else {
+  DB_URI = "lunchly"
+}
 
-db.connect();
+// These are default credentials that should be replaced 
+const db = new Client({
+  user: 'N',
+  host: 'localhost',
+  password: 'abc123',
+  database: DB_URI,
+  port: 5432, 
+});
 
-module.exports = db;
+// Connect to the database
+db.connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch(err => console.error('Error connecting to PostgreSQL database', err));
+
+module.exports = db
